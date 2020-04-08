979777dbc4585072e25a5bb44eb997372e1af3d7
lxy <921803796@qq.com>
2020-04-08 19:43:12 +0800

    修改字符集

--- src/com/rj/bd/user/action/UserAction.java
+++ src/com/rj/bd/user/action/UserAction.java
@@ -1,12 +1,12 @@
 package com.rj.bd.user.action;
 /**
 * @author dell
-* @version 创建时间：2020年4月8日 下午6:55:05
-* 说明:
+* @version 锟斤拷锟斤拷时锟戒：2020锟斤拷4锟斤拷8锟斤拷 锟斤拷锟斤拷6:55:05
+* 说锟斤拷:
 */
 public class UserAction {
 
 	public void hi(){
-		System.out.println("小张说：111");
+		
 	}
 }
