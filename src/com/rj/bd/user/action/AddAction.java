fdab1c50a57ddf72e71daf9238432902813911f9
lxy <921803796@qq.com>
2020-04-09 12:47:14 +0800

    求和action

---
+++ src/com/rj/bd/user/action/AddAction.java
@@ -0,0 +1,11 @@
+package com.rj.bd.user.action;
+/**
+* @author dell
+* @version 创建时间：2020年4月9日 下午12:44:13
+* 说明:
+*/
+public class AddAction {
+	public void add(){
+		System.out.println("求和....");
+	}
+}
